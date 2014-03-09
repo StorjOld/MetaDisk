2183f18e7cf7e8693c7d00a984d55f8a6dd2ab5d
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-03-09 21:06:02 +0000

    Adds a blockchain synchronization script.
    Be careful when you use worker.py. It
    actually communicates with the datacoin server
    and makes transactions.

---
+++ worker.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Warning: This script actually sends data
+# to the blockchain, spending your coins.
+
+import cloudmanager
+import metachains_dtc
+
+import settings
+
+coin = metachains_dtc.Datacoin(
+        settings.DATACOIN_URL,
+        settings.DATACOIN_USERNAME,
+        settings.DATACOIN_PASSWORD)
+
+cloud = cloudmanager.CloudManager(
+        settings.DATABASE
+        settings.STORAGE_PATH,
+        settings.STORAGE_SIZE)
+
+worker = metachains_dtc.Synchronizer(
+        coin,
+        cloud,
+        settings.DATACOIN_START)
+
+
+print "scanning blockchain.."
+worker.scan_blockchain()
+
+print "scanning database.."
+worker.scan_database()
