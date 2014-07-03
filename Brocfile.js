e253e1534899ce085aac44be37704975d5159ffe
Josh Brandoff <josh@joshua.is>
2014-07-02 21:56:45 -0400

    Added IndexedDB Adapter for File Data

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,8 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('vendor/ember-indexeddb-adapter/dist/ember_indexeddb_adapter.js', {
+	'exports': {'ember-indexeddb-adapter': [ 'default' ]}
+});
+
 module.exports = app.toTree();
