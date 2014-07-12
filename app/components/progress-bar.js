41557d0a025ebcc7d994d143d8a7895214b98707
Josh Brandoff <josh@joshua.is>
2014-07-12 05:05:26 -0400

    html5 progress bars/components animate on value change

--- app/components/progress-bar.js
+++ app/components/progress-bar.js
@@ -2,5 +2,15 @@ export default Ember.Component.extend({
 	tagName: 'progress',
 	attributeBindings: ['max', 'value'],
 	max: null,
-	value: null
+	toValue: null,
+	value: 0,
+	animateValue: function() {
+		var that = this;
+		this.$({barValue: that.get('value'), that: that}).animate({barValue: that.get('toValue')}, {
+			duration: 250,
+			step: function() {
+				this.that.set('value', this.barValue);
+			}
+		});
+	}.property('toValue')
 });
