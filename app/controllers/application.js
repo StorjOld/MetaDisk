7ed907e8bb93cecd4fb25cc2d949df483e932a2d
Josh Brandoff <josh@joshua.is>
2014-07-12 06:27:16 -0400

    Integrated support for HTML5 Notifications

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -30,5 +30,35 @@ export default Ember.Controller.extend({
 				maxFileSize: data.storage.max_file_size
 			});
 		}
+	},
+
+	actions: {
+		notify: function(subject, body) {
+			if (Notification && Notification.permission !== 'granted') {
+				Notification.requestPermission(function(status) {
+					if (Notification.permission !== status) {
+						Notification.permission = status;
+					}
+				});
+			} else if (Notification && Notification.permission === 'granted') {
+				var notification = new Notification(subject, {body: body, icon: '/assets/images/storj.ico'});
+				setTimeout(function(){ notification.close(); }, 4000);
+			} else if (Notification && Notification.permission !== 'denied') {
+				Notification.requestPermission(function(status) {
+					if (Notification.permission !== status) {
+						Notification.permission = status;
+					}
+
+					if (status === 'granted') {
+						var notification = new Notification(subject, {body: body, icon: '/assets/images/storj.ico'});
+						setTimeout(function(){ notification.close(); }, 4000);
+					} else {
+						alert(body);
+					}
+				})
+			} else {
+				alert(body);
+			}
+		}
 	}
-})
+});
