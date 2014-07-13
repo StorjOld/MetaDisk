bc18361b68b3277b339b197233b6cb11a269486c
Josh Brandoff <josh@joshua.is>
2014-07-13 17:39:20 -0400

    Allow file listing to expand/contract

--- app/components/file-listing.js
+++ app/components/file-listing.js
@@ -1,6 +1,13 @@
 export default Ember.Component.extend({
 	tagName: 'section',
 	classNames: ['file'],
+	classNameBindings: ['isClosed:closed:'],
 	title: null,
-	layout: Ember.Handlebars.compile('<h1>{{title}}</h1><fieldset><button></button><button></button><button></button</fieldset>')
+	isClosed: false,
+	layout: Ember.Handlebars.compile('<h1 {{action "toggleClosed"}}>{{title}}</h1><fieldset><button></button><button></button><button></button</fieldset>'),
+	actions: {
+		toggleClosed: function() {
+			this.toggleProperty('isClosed');
+		}
+	}
 });
