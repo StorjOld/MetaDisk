3a131910edbf9dfe627ff2ea7ed3c270ef7c05f7
Josh Brandoff <josh@joshua.is>
2014-07-12 23:40:54 -0400

    Added field for file name/title

--- app/models/hash-key.js
+++ app/models/hash-key.js
@@ -2,6 +2,7 @@ export default DS.Model.extend({
 	token: DS.belongsTo('token'),
 	hash: DS.attr('string'),
 	key: DS.attr('key'),
+	title: DS.attr('string'),
 	uri: function() {
 		return this.get('hash') + '?key=' + this.get('key') + '&token=' + this.get('token').get('token');
 	}.property('hash', 'key')
