d2800a521cdb05c48a9a8a593c3e6435f124804e
Josh Brandoff <josh@joshua.is>
2014-07-13 13:59:06 -0400

    Added default bandwidth

--- app/models/token.js
+++ app/models/token.js
@@ -1,6 +1,6 @@
 export default DS.Model.extend({
 	token: DS.attr('string'),
-	yourBandwidth: DS.attr('number'),
+	yourBandwidth: DS.attr('number', {default: 0}),
 	estimatedStorage: function() {
 		return this.get('yourBandwidth') / 3;
 	}.property('yourBandWidth'),
