d17be51f0f9acf0d547ef169510e1c2a7fbddfdf
Josh Brandoff <josh@joshua.is>
2014-07-13 19:23:31 -0400

    File conditionally shows progress bar until fully uploaded

--- app/components/file-listing.js
+++ app/components/file-listing.js
@@ -6,13 +6,17 @@ export default Ember.Component.extend({
 	fileHash: null,
 	fileKey: null,
 	fileUri: null,
-	progressValue: null,
-	uploadStatus: null,
+	fileSize: 9283408,
+	bytesUploaded: 9000000,
 	isClosed: false,
-	layout: Ember.Handlebars.compile('<h1 {{action "toggleClosed"}}>{{title}}</h1><fieldset><button></button><button></button><button></button</fieldset>'),
+	isCompletelyUploaded: function() {
+		return this.get('fileSize') === this.get('bytesUploaded');
+	}.property('fileSize', 'bytesUploaded'),
+	layout: Ember.Handlebars.compile('<h1 {{action "toggleClosed"}}>{{title}}</h1>{{#animated-if condition=isCompletelyUploaded}}<fieldset><button></button><button></button><button></button></fieldset>{{/animated-if}}{{#animated-unless condition=isCompleteyUploaded}}{{progress-bar max=fileSize toValue=bytesUploaded}}{{/animated-unless}}'),
 	actions: {
 		toggleClosed: function() {
 			this.toggleProperty('isClosed');
 		}
 	}
 });
+
