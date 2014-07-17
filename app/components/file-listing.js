8e56b71924b5ba77a81a5e64b74abc1825baff46
Josh Brandoff <josh@joshua.is>
2014-07-17 04:55:10 -0400

    Added additional info to file upload progress bar

--- app/components/file-listing.js
+++ app/components/file-listing.js
@@ -9,12 +9,19 @@ export default Ember.Component.extend({
 	fileSize: null,
 	bytesUploaded: null,
 	isClosed: false,
+	downloadUri: null,
+	uploadedPercent: function() {
+		return Math.min(100, Math.floor(this.get('bytesUploaded')/this.get('fileSize') * 100));
+	}.property('bytesUploaded', 'fileSize'),
 	isCompletelyUploaded: function() {
 		return this.get('fileSize') === this.get('bytesUploaded');
 	}.property('fileSize', 'bytesUploaded'),
 	actions: {
 		toggleClosed: function() {
 			this.toggleProperty('isClosed');
+		},
+		downloadFile: function() {
+			this.set('downloadUri', '/api/download/' + this.get('fileHash') + '?key=' + this.get('fileKey') + '?token=');
 		}
 	}
 });
