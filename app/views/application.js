d549e57d6dd7f0b1821f331792efd69dfda6b416
Josh Brandoff <josh@Emergent-Gestalt.local>
2014-08-24 22:34:10 -0400

    Added view to handle body drag/drop events

---
+++ app/views/application.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  classNames: ['drop-zone'],
+  classNameBindings: ['isActive:active:'],
+  isActive: false,
+
+  dragEnter: function(e) {
+  	e.stopPropagation();
+  	e.preventDefault();
+  },
+
+  dragOver: function(e) {
+  	this.set('isActive', true);
+  	e.stopPropagation();
+  	e.preventDefault();
+  },
+
+  drop: function(e) {
+  	this.set('isActive', false);
+  	e.stopPropagation();
+  	e.preventDefault();
+
+  	var dt = e.dataTransfer;
+  	var files = dt.files;
+
+  	if (files.length > 0) {
+		for (var i=0; i < files.length; i++) {
+			this.get('controller').send('handleFiles', files[i]);
+		}
+	}
+  	
+  }
+});
