805afcbdbec46b2f04593a470417ce1541fc6d6a
Josh Brandoff <josh@Emergent-Gestalt.local>
2014-08-27 00:09:36 -0400

    Added try/catch to file listing step function if ember object destroyed  This should be something cleaner.

--- app/components/progress-bar.js
+++ app/components/progress-bar.js
@@ -11,8 +11,15 @@ export default Ember.Component.extend({
 		this.$({barValue: that.get('value'), that: that}).animate({barValue: that.get('toValue')}, {
 			duration: 250,
 			step: function() {
-				this.that.set('value', this.barValue);
+				try {
+					this.that.set('value', this.barValue);
+				} catch(err) { 
+					// This should ultimately be replaced with something 
+					// cleaner. Ember's willDestroyElement and willClearRender 
+					// hooks in combination with this.$().finish() or 
+					// this.$().stop(true, true) seem to have no effect.
+				}
 			}
-		});
+		});	
 	}.observes('toValue')
 });
