4c64a2f63d2ca9bfeb6b4d13b18219949bfa4d38
Josh Brandoff <josh@Emergent-Gestalt.local>
2014-09-09 20:39:49 -0400

    Pull module prefix from configuration

--- app/app.js
+++ app/app.js
@@ -5,8 +5,7 @@ import loadInitializers from 'ember/load-initializers';
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  modulePrefix: 'metadisk', // TODO: loaded via config
-  Resolver: Resolver,
+  modulePrefix: config.modulePrefix,
   Socket: EmberSockets.extend({
   	host: window.API_HOST_NAME, //TODO: should be configurable
   	path: 'metadisk',
@@ -14,6 +13,6 @@ var App = Ember.Application.extend({
   })
 });
 
-loadInitializers(App, 'metadisk');
+loadInitializers(App, config.modulePrefix);
 
 export default App;
