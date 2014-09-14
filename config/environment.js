0c7352518c5399a54e7696ec78b9eadcc9753a7d
Josh Brandoff <josh@Emergent-Gestalt.local>
2014-09-14 19:08:52 -0400

    Set app settings inside environmental config

--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,8 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+      API_HOST_NAME: 'node3.metadisk.org',
+      ACCOUNTS_ENABLED: false
     }
   };
 
